abcb7b6272ee37fdb2f7ecdede306630abee223a
Sajid <yeamimhossainsajid@gmail.com>
2025-05-22 12:08:23 +0600

    url updated

--- src/main/java/com/example/BigganGlopo/features/subtopic/controller/SubTopicController.java
+++ src/main/java/com/example/BigganGlopo/features/subtopic/controller/SubTopicController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,6 +20,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
+@RequestMapping("/subtopic")
 public class SubTopicController extends AbstractController<SubTopic, SubTopicRequestDto, GenericSearchDto> {
 
     @Autowired
